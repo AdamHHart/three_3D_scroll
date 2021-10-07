d617c0f0cfd1fee5bc872160946370932630f21c
Adam Hart <adamhayhart@gmail.com>
2021-10-07 13:29:26 -0700

    canvas added within container element, fixed to top

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+import Sketch from "./module";
+
+let sketch = new Sketch({
+  dom: document.getElementById("container"),
+});
+
 let speed = 0;
 let position = 0;
 let rounded = 0;
 let block = document.getElementById("block");
+let wrap = document.getElementById("wrap");
 let elems = [...document.querySelectorAll(".n")];
 
 window.addEventListener("wheel", (e) => {
@@ -17,9 +24,9 @@ function raf() {
   speed *= 0.8;
 
   objs.forEach((o, i) => {
-    o.dist = position - i;
-    o.dist = Math.abs(o.dist);
-    elems[i].style.transform = `scale(${o.dist})`;
+    o.dist = Math.min(Math.abs(position - i), 1);
+    o.dist = 1 - o.dist ** 2;
+    elems[i].style.transform = `scale(${1 + 0.4 * o.dist})`;
   });
 
   rounded = Math.round(position);
@@ -28,7 +35,8 @@ function raf() {
 
   position += Math.sign(diff) * Math.pow(Math.abs(diff), 0.7) * 0.015;
 
-  block.style.transform = `translate(0, ${position * 100 + 50}px)`;
+  // block.style.transform = `translate(0, ${position * 100 + 50}px)`;
+  wrap.style.transform = `translate(0, ${-position * 100 + 50}px)`;
   window.requestAnimationFrame(raf);
 }
 
--- dist/index.fc614997.js
+++ dist/index.fc614997.js
@@ -459,6 +459,7 @@ let speed = 0;
 let position = 0;
 let rounded = 0;
 let block = document.getElementById("block");
+let wrap = document.getElementById("wrap");
 let elems = [
     ...document.querySelectorAll(".n")
 ];
@@ -474,14 +475,15 @@ function raf() {
     position += speed;
     speed *= 0.8;
     objs.forEach((o, i)=>{
-        o.dist = position - i;
-        o.dist = Math.abs(o.dist);
-        elems[i].style.transform = `scale(${o.dist})`;
+        o.dist = Math.min(Math.abs(position - i), 1);
+        o.dist = 1 - o.dist ** 2;
+        elems[i].style.transform = `scale(${1 + 0.4 * o.dist})`;
     });
     rounded = Math.round(position);
     let diff = rounded - position;
     position += Math.sign(diff) * Math.pow(Math.abs(diff), 0.7) * 0.015;
-    block.style.transform = `translate(0, ${position * 100 + 50}px)`;
+    // block.style.transform = `translate(0, ${position * 100 + 50}px)`;
+    wrap.style.transform = `translate(0, ${-position * 100 + 50}px)`;
     window.requestAnimationFrame(raf);
 }
 raf();
