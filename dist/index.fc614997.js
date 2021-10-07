9a01069327676a93c016c3a05293e2846042608b
Adam Hart <adamhayhart@gmail.com>
2021-10-07 12:57:48 -0700

    very nice scrol animation changing the scale of objects as you scroll

--- dist/index.fc614997.js
+++ dist/index.fc614997.js
@@ -457,16 +457,31 @@ function hmrAcceptRun(bundle, id) {
 },{}]},["e2Fed"], null, "parcelRequire94c2")
 let speed = 0;
 let position = 0;
+let rounded = 0;
 let block = document.getElementById("block");
+let elems = [
+    ...document.querySelectorAll(".n")
+];
 window.addEventListener("wheel", (e)=>{
-    console.log("e = ", e);
-    speed += e.deltaY * 0.0002;
+    // console.log("e = ", e);
+    speed += e.deltaY * 0.0003;
+});
+let objs = Array(5).fill({
+    dist: 0
 });
 function raf() {
-    console.log("speed = ", speed);
+    // console.log("speed = ", speed);
     position += speed;
     speed *= 0.8;
-    block.style.transform = `translate(0, ${position * 100}px)`;
+    objs.forEach((o, i)=>{
+        o.dist = position - i;
+        o.dist = Math.abs(o.dist);
+        elems[i].style.transform = `scale(${o.dist})`;
+    });
+    rounded = Math.round(position);
+    let diff = rounded - position;
+    position += Math.sign(diff) * Math.pow(Math.abs(diff), 0.7) * 0.015;
+    block.style.transform = `translate(0, ${position * 100 + 50}px)`;
     window.requestAnimationFrame(raf);
 }
 raf();
